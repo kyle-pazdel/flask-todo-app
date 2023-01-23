628615e32cc607d82675e800f52216f07f875af0
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-23 13:56:12 -0800

    Renders index.html to app using flask render_template

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Web App with Python Flask!'
+    name = 'Kyle'
+    return render_template('index.html', title='Welcome', username=name)
 
 
 app.run(host='0.0.0.0', port=81)
