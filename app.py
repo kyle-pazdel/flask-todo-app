205f39bbd6dc8b10e922ce39b060ca629df1ca35
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-23 19:04:05 -0800

    Adds a synthesized call to a backend with params to display an object's value based on key in url

--- app.py
+++ app.py
@@ -12,4 +12,23 @@ def index():
     return render_template('index.html', title=title, members=users)
 
 
+# Sample route
+@app.route('/hello')
+def hello_world():
+    return "hello world"
+
+# Sample route with param
+
+
+# posts = [{"id": 1, "1": "Post 1", "body": "The body text of Post 1"},
+#          {"id": 2, "2": "Post 2", "body": "The body text of Post 2"},
+#          {"id": 3, "3": "Post 3", "body": "The body text of Post 3"}]
+post = {"id": 1, "1": "Post 1", "body": "The body text of Post 1"}
+
+
+@app.route('/posts/<id>')
+def show_post(id):
+    return f"Title: {post[str(id)]}"
+
+
 app.run(host='0.0.0.0', port=81)
